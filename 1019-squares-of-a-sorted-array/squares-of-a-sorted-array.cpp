f199a235a8b0e2c1aa91cf60493b6dd6b07db639
Mohitbagul <126978039+Mohitbagul@users.noreply.github.com>
2024-03-02 16:22:28 +0530

    Time: 15 ms (94.10%) | Memory: 28.4 MB (58.32%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-       for(int i=0;i<nums.size();i++){
-           nums[i]=(nums[i]*nums[i]);
-       } 
-       sort(nums.begin(),nums.end());
-       return nums;
+        int a=0,b=nums.size()-1;
+
+        vector<int>v(nums.size());
+
+        for(int i=nums.size()-1;i>=0;i--){
+            if(abs(nums[a])>=abs(nums[b])){
+                v[i]=nums[a]*nums[a];
+                a++;
+            }
+            else{
+                v[i]=nums[b]*nums[b];
+                b--;
+            }
+        }  
+        return v;
     }
 };
