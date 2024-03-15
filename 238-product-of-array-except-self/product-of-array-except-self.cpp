ab78beb2a708d45a34326b49330c2568c578f590
Mohitbagul <126978039+Mohitbagul@users.noreply.github.com>
2024-03-16 00:08:46 +0530

    Time: 18 ms (42.06%) | Memory: 26.8 MB (47.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        long long x=1;
-        int cnt0=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) cnt0++;
-            else
-            x=x*nums[i];
-        }
+        int cnt=nums[0];
+        int arr1[nums.size()];
+        int arr2[nums.size()];
 
+        arr1[0]=nums[0];
+        arr2[nums.size()-1]=nums[nums.size()-1];
+
+        for(int i=1;i<nums.size();i++){
+            cnt = cnt*nums[i];
+            arr1[i]=cnt;
+        }
+        cnt=nums[nums.size()-1];
+        for(int i=nums.size()-2;i>=0;i--){
+            cnt = cnt*nums[i];
+            arr2[i]=cnt;
+        }
         for(int i=0;i<nums.size();i++){
-            if(cnt0==0){
-                nums[i]=(x/nums[i]);
+            if(i==0){
+                nums[i]=arr2[1];
             }
-            else if(cnt0==1){
-                if(nums[i]!=0){
-                    nums[i]=0;
-                }
-                else{
-                    nums[i]=(x);
-                }
+            else if(i==nums.size()-1){
+                nums[i]=arr1[nums.size()-2];
             }
             else{
-                nums[i]=0;
-            }  
-            
+                nums[i]=arr1[i-1]*arr2[i+1];
+            }
         }
         return nums;
     }
