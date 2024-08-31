db19f188733d6a7f9f4e8cc6ba463edfdf857b7c
Mohitbagul <126978039+Mohitbagul@users.noreply.github.com>
2024-08-31 16:10:13 +0530

    Time: 3 ms (49.39%) | Memory: 7.8 MB (93.31%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-
-    void solve(vector<vector<int>>&ans,vector<int>v,int n,int k,int ind,int arr[]){
-
-        if(v.size()>k || n<0) return;
+    void solve(vector<vector<int>>&ans,vector<int>&v,int n,int k,int ind,int arr[]){
+        if(n<0 || v.size()>k)
+        return;
 
         if(v.size()==k && n==0){
             ans.push_back(v);
             return;
         }
-        if(ind==9) return;
-        cout<<arr[ind]<<" ";
 
-        v.push_back(arr[ind]);
-        solve(ans,v,n-arr[ind],k,ind+1,arr);
-        v.pop_back();
-        solve(ans,v,n,k,ind+1,arr);
+        for(int i=ind;i<9;i++){
+            v.push_back(arr[i]);
+            solve(ans,v,n-arr[i],k,i+1,arr);
+            v.pop_back();
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         int arr[]={1,2,3,4,5,6,7,8,9};
