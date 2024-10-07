6f6e321e01409e78fdc749cecd2d7155e98d8e8e
Mohit Bagul <126978039+Mohitbagul@users.noreply.github.com>
2024-10-08 00:40:03 +0530

    Time: 354 ms (43.45%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,11 +1,13 @@
 SELECT t1.visited_on AS visited_on
       ,SUM(amount) AS amount
       ,ROUND(SUM(amount) / 7, 2) AS average_amount 
-FROM (SELECT DISTINCT(visited_on) AS visited_on
-      FROM Customer
-      WHERE DATE_SUB(visited_on, INTERVAL 6 DAY) IN (SELECT DISTINCT(visited_on)
-                                                     FROM Customer)) AS t1
+FROM ( SELECT DISTINCT visited_on 
+    FROM Customer
+    WHERE DATEDIFF(
+        visited_on, 
+        (SELECT MIN(visited_on) FROM Customer)
+    ) >= 6 ) AS t1
 LEFT JOIN Customer AS t2
 ON t2.visited_on BETWEEN DATE_SUB(t1.visited_on, INTERVAL 6 DAY) AND t1.visited_on
 GROUP BY t1.visited_on
-;
+HAVING COUNT(t1.visited_on) >= 7
