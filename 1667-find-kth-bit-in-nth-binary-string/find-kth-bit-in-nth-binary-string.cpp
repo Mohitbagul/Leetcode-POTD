e8b930ca8d239813e00cfaf1e686e3dd92dc364e
Mohit Bagul <126978039+Mohitbagul@users.noreply.github.com>
2024-10-19 23:34:34 +0530

    Time: 52 ms (23.55%) | Memory: 59.4 MB (35.16%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
+    string sol2(string s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            swap(s[i++],s[j--]);
+        }
+
+        for(i=0;i<s.length();i++){
+            if(s[i]=='1')s[i]='0';
+            else s[i]='1';
+        }
+        return s;
+    }
+
+    string sol(int n){
+        if(n==1)return "0";
+
+        string s = sol(n-1);
+        return s+"1"+sol2(s);
+
+    }
+
     char findKthBit(int n, int k) {
-        if(n==1) return '0';
-        
-        int length = (1<<n) - 1;
-
-        int mid = length/2 + 1;
-
-        if(k==mid) return '1';
-
-        if(k<mid) return findKthBit(n-1,k);
-
-        return findKthBit(n-1,length-k+1) == '0' ? '1' : '0';
+        string s = sol(n);
+        return s[k-1];
     }
 };
